d48ccba4c7b2013b22bc2611fc23f391c8ed568b
Hector Saul Maldonado Gomez <saul_99_27@hotmail.com>
2020-11-26 00:04:10 -0600

    Avance Actividad 1 Control de Usuarios

---
+++ ActividadesU4PrograWeb/Actividad1ControlCuentasUsuario/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Actividad1ControlCuentasUsuario.Models
+{
+    public partial class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string NombreCompleto { get; set; }
+        public string Correo { get; set; }
+        public string Contrasena { get; set; }
+    }
+}
---
+++ ActividadesU4PrograWeb/Actividad1ControlCuentasUsuario/Models/controlusuariosContext.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Actividad1ControlCuentasUsuario.Models
+{
+    public partial class controlusuariosContext : DbContext
+    {
+        public controlusuariosContext()
+        {
+        }
+
+        public controlusuariosContext(DbContextOptions<controlusuariosContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Usuario> Usuario { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseMySql("server=localhost;user=root;password=root;database=controlusuarios", x => x.ServerVersion("5.7.18-mysql"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.HasKey(e => e.IdUsuario)
+                    .HasName("PRIMARY");
+
+                entity.ToTable("usuario");
+
+                entity.Property(e => e.IdUsuario)
+                    .HasColumnName("idUsuario")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.Contrasena)
+                    .IsRequired()
+                    .HasColumnType("varchar(100)")
+                    .HasCharSet("utf8")
+                    .HasCollation("utf8_general_ci");
+
+                entity.Property(e => e.Correo)
+                    .IsRequired()
+                    .HasColumnType("varchar(100)")
+                    .HasCharSet("utf8")
+                    .HasCollation("utf8_general_ci");
+
+                entity.Property(e => e.NombreCompleto)
+                    .IsRequired()
+                    .HasColumnType("varchar(100)")
+                    .HasCharSet("utf8")
+                    .HasCollation("utf8_general_ci");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
